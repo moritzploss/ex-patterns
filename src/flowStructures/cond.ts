1959fbe89c8acd4ceafe89f1a7a41e7f044a2b60
Moritz Ploss <moritz.ploss@relevant-digital.com>
2020-05-05 20:11:44 +0200

    add tests for cond statement

--- src/flowStructures/cond.ts
+++ src/flowStructures/cond.ts
@@ -1,12 +1,17 @@
 import { end, End } from '../symbols';
 
 const _cond = (done = false, result = null) => (
-  (value: any | End, returnValue?: any): any => {
+  // eslint-disable-next-line func-names
+  function (value: any | End, returnValue?: any): any {
     if (value === end) {
       if (done) {
         return result;
       }
       throw Error('No matching clause found. ');
+    }
+
+    if (arguments.length !== 2) {
+      throw Error('Expected 1 or 2 arguments of type (end) or (pattern, function).');
     }
 
     if (done) {
--- test/cond.test.ts
+++ test/cond.test.ts
@@ -9,7 +9,7 @@ import { expect } from 'chai';
 import { cond, then, end } from '../src';
 
 describe('the when function: base cases', () => {
-  it('should match the first matching value (1)', () => {
+  it('should match the first matching value', () => {
     const fizzBuzz = (number) => cond
       (number % 15 === 0, then('fizzbuzz'))
       (number % 3 === 0, then('fizz'))
@@ -19,5 +19,63 @@ describe('the when function: base cases', () => {
 
     const result = [1, 2, 3, 4, 5, 7, 15, 16].map(fizzBuzz);
     expect(result).to.deep.equal([1, 2, 'fizz', 4, 'buzz', 7, 'fizzbuzz', 16]);
+  });
+
+  it('should throw if called with only one argument that is not \'end\'', () => {
+    const fizzBuzz = (number) => cond
+      (number % 15 === 0, then('fizzbuzz'))
+      (number % 3 === 0, then('fizz'))
+      (number % 5 === 0, then('buzz'))
+      (true, then(number))
+    ('end');
+
+    const func = () => fizzBuzz(5);
+    expect(func).to.throw(Error);
+  });
+
+  it('should throw if no matching clause is found', () => {
+    const fizzBuzz = (number) => cond
+      (number % 15 === 0, then('fizzbuzz'))
+      (number % 3 === 0, then('fizz'))
+      (number % 5 === 0, then('buzz'))
+    (end);
+
+    const func = () => fizzBuzz(1);
+    expect(func).to.throw(Error);
+  });
+
+  it('should adhere to JavaScript\'s concept of falsiness', () => {
+    const result = cond
+      (false, then('no match'))
+      (0, then('no match'))
+      (-0, then('no match'))
+      ('', then('no match'))
+      (null, then('no match'))
+      (undefined, then('no match'))
+      (NaN, then('no match'))
+      (1, then('this one matches'))
+    (end);
+
+    expect(result).to.deep.equal('this one matches');
+  });
+
+  it('should adhere to JavaScript\'s concept of truthiness', () => {
+    const result = cond
+      (!true, then('no match'))
+      (!{}, then('no match'))
+      (![], then('no match'))
+      (!42, then('no match'))
+      (!'0', then('no match'))
+      (!'false', then('no match'))
+      (!new Date(), then('no match'))
+      (!-42, then('no match'))
+      (!3.14, then('no match'))
+      (!-3.14, then('no match'))
+      (!Infinity, then('no match'))
+      (!-Infinity, then('no match'))
+      (true, then('this one matches'))
+    (end);
+
+    expect(result).to.deep.equal('this one matches');
   });
 });
