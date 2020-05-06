84995310a5cd6b05c305c1a1e6620c958b967194
Moritz <moritz.ploss@gmail.com>
2020-05-06 22:10:04 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  "roots": [
-      "<rootDir>/test"
+  roots: [
+    '<rootDir>/test',
   ],
-  "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-  }
-}
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0,
+    },
+  },
+};
